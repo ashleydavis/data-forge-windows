f4f0f5fa32884636c1ff1d4985736361f8c83db9
Ashley Davis <ashley@codecapers.com.au>
2022-09-10 08:12:20 +1000

    Grouping prototype.

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
+const moment = require("moment");
 const dataForge = require("data-forge");
 require("data-forge-fs");
 
-const df = dataForge.readFileSync("data.json").parseJSON();
 
-// Here is where I would window the data and add the runnong sum of the premium column as a new column
-// But I've no idea how that would work
-// So, for this repo I'm just doing s standard rolling window here cribbed from the example
-const windows = df.rollingWindow(50);
-const newCol = windows
-	.select((window, windowIndex) => {
-		return [windowIndex, window.getSeries("premium").sum()];
-	})
-	.withIndex((pair) => pair[0])
-	.select((pair) => pair[1]);
+let df = dataForge.readFileSync("data.json")
+    .parseJSON()
+    .transformSeries({
+        date: v => new Date(v), // Parse date from integer.
+    })
+    .orderBy(row => row.date); // Make sure rows are in cronological order.
 
-console.log(newCol.tail(10).toString());
+//
+// Group the data into hourly buckets.
+//
+df = df.groupBy(row => moment(row.date).format("YYYY-MM-DD-HH")) // Result of groupBy is a series.
+    .select(group => {
+        return { // Transform each group you you want.
+            date: group.first().date,
+            groupKey: moment(group.first().date).format("YYYY-MM-DD-HH"),
+            sum: group.getSeries("premium").sum(),
+        };
+    })
+    .inflate(); // Convert to a dataframe.
+
+const preview = df.take(100).toStrings({ date: "YYYY-MM-DD hh:mm:ss" });
+console.log(preview.toString());
